f9f5c57635c06806214d95047141127b331190e3
jesusfb <jfigueroa.beltran@gmail.com>
2025-02-26 09:25:38 -0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Asmin',
+            'email' => 'jfb@jfb.com',
+            'password' => bcrypt('1234qwer'),
+        ]);
+    }
+}
